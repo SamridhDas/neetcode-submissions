b4054a37331b4142ea043e75d012924121884e40
SamridhDas <samridhdas@gmail.com>
2026-06-24 13:12:03 +0530

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res=[]
+        wordSet = set(wordDict)
+        def dfs(start,path):
+            if start==len(s):
+                res.append(" ".join(path[:]))
+                return 
+            for i in range(start,len(s)):
+                if s[start:i+1] in wordSet:
+                    path.append(s[start:i+1])
+                    dfs(i+1,path)
+                    path.pop()
+        dfs(0,[])
+        return res
+                
